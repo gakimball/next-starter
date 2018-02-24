0af937fc7e3850069d48f2e78133249b10ed3d7f
Geoff Kimball <geoff@geoffkimball.com>
2018-02-23 16:58:38 -0800

    Add documentation to provider

--- @ueno/starter-kit/provider.js
+++ @ueno/starter-kit/provider.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'mobx-react';
 
+/**
+ * Create a decorator that adds MobX support to your page. If you're using MobX, you'll want to
+ * use this decorator on every page component.
+ * @param {Object.<String, Function>} stores - Stores to provide.
+ * @returns {ProviderFunction} Decorator function.
+ */
 export default (stores) => {
   let store;
 
+  /**
+   * Wrapper class to hold all stores. Initial state can be passed to the wrapper constructor,
+   * and store-specific state will be passed to each store constructor.
+   * @name StoreContainer
+   * @private
+   * @constructor
+   * @param {Object} state - Initial state.
+   */
   function StoreContainer(state) {
     Object.entries(stores).forEach(([name, Store]) => {
       this[name] = new Store(state[name]);
     });
   }
 
+  /**
+   * Get a store container. On the server, a new container is returned each time. On the client,
+   * the first time a container is fetched, a new one will be created and cached. On subsequent
+   * requests, the cached container will be used. This is because we want to persist the store
+   * between page transitions.
+   * @private
+   * @param {Boolean} [isServer=false] - Server is asking for a store container.
+   * @param {Object} [state={}] - Initial state to give to container stores.
+   * @returns {StoreContainer} Store container.
+   */
   const getStores = (isServer = false, state = {}) => {
     if (isServer && typeof window === 'undefined') {
       return new StoreContainer(state);
@@ -23,11 +47,17 @@ export default (stores) => {
     return store;
   };
 
+  /**
+   * Decorate a component to add MobX support.
+   * @callback ProviderFunction
+   * @param {Function} Target - Component to decorate.
+   * @returns {Function} Decorated component.
+   */
   return Target => class Page extends Component {
 
     static async getInitialProps(ctx) {
       const isServer = !!ctx.req;
-      const storeData = {};
+      const storeData = {}; // @TODO Figure out how this works, or if it's needed
       const props = { isServer, storeData };
 
       if (typeof Target.getInitialProps === 'function') {
