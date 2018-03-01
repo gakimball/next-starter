2c0eaff23a7071a0e882eece14a5174cfcd6725d
Geoff Kimball <geoff@geoffkimball.com>
2018-02-28 18:11:06 -0800

    Fix Babel eslint issues and change pages/index.js to be a class-based component

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,38 @@ import React from 'react';
 import { inject, PropTypes as MobxPropTypes } from 'mobx-react';
 import getConfig from 'next/config';
 import Header from '../components/header';
+import Layout from '../components/layout';
 import withStores from '../stores';
 import './index.scss';
 
 const { publicRuntimeConfig: config } = getConfig();
 
-const Index = ({ planets = [] }) => (
-  <div>
-    <Header />
-    <p>Nothing feels like ::ffff</p>
-    <p>{config.host}:{config.port}</p>
-    <ul>
-      {planets.map(planet => (
-        <li key={planet}>
-          {planet}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-
-Index.propTypes = {
-  planets: MobxPropTypes.observableArray.isRequired, // eslint-disable-line react/no-typos
-};
-
-const injector = stores => ({
+@withStores
+@inject(stores => ({
   planets: stores.planets.planets,
-});
+}))
+export default class Index extends React.Component {
 
-export default withStores(inject(injector)(Index));
+  static propTypes = {
+    planets: MobxPropTypes.observableArray.isRequired, // eslint-disable-line react/no-typos
+  }
+
+  render() {
+    const { planets } = this.props;
+
+    return (
+      <Layout title="Home!!">
+        <Header />
+        <p>Nothing feels like ::ffff</p>
+        <p>{config.host}:{config.port}</p>
+        <ul>
+          {planets.map(planet => (
+            <li key={planet}>
+              {planet}
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    );
+  }
+}
